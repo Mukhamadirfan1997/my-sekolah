b460d2030323c0ed755fc24e5fb02bb567655296
Mukhamadirfan1997 <154989281+Mukhamadirfan1997@users.noreply.github.com>
2025-02-17 15:40:09 +0700

    upload pertama

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+
+import HomePage from "./Pages/HomePage.jsx";
+
+function App() {
+
+  return <div>
+      {/*Navbar*/}
+      <Navbar />
+      {/*Content*/}
+      <HomePage />
+      {/*Footer*/}
+      <Footer />
+  </div>
+}
+
+export default App
